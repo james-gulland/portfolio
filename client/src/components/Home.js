9a04c7793ab6f38bb96c73cf07a111c30858543d
James Gulland <jamesgulland@gmail.com>
2023-05-02 12:27:47 +0100

    updating intro section

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,11 +5,16 @@ const Home = () => {
   return (
     <>
       <div id="intro-container">
+        <div id="current-status" className="pill"><span id="pill-emoji">☻</span> Open to work</div>
         <div id="profile-pic">
           <img src={profilePic} alt="Profile" />
         </div>
         <p className="heading">Hello there!</p>
-        <p id="profile-description" className="heading">I&apos;m James, a developer with a background in product management.</p>
+        <p id="profile-description" className="heading">I&apos;m James, a full-stack <span className="accent-colour">developer</span> with a background in product management.</p>
+        <div id="contact-buttons">
+          <div id="contact-email" className="button">Email</div>
+          <div id="contact-email" className="button">LinkedIn</div>
+        </div>
       </div>
     
     </>
