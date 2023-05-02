5dae2fe5f98bee51dff746f6fb230a8d30f0ed5c
James Gulland <jamesgulland@gmail.com>
2023-05-02 13:24:21 +0100

    adding contact links

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,8 @@
 import profilePic from '../assets/me.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
@@ -12,8 +16,12 @@ const Home = () => {
         <p className="heading">Hello there!</p>
         <p id="profile-description" className="heading">I&apos;m James, a full-stack <span className="accent-colour">developer</span> with a background in product management.</p>
         <div id="contact-buttons">
-          <div id="contact-email" className="button">Email</div>
-          <div id="contact-email" className="button">LinkedIn</div>
+          <Link className="profile" to={'mailto:jamesgulland@gmail.com'} as={Link}>
+            <div id="contact-email" className="button"><FontAwesomeIcon className="icon" icon={faEnvelope} />Email</div>
+          </Link>
+          <Link className="profile" to={'https://www.linkedin.com/in/jamesgulland/'} as={Link}>
+            <div id="contact-linkedIn" className="button"><FontAwesomeIcon className="icon" icon={faLinkedin} />LinkedIn</div>
+          </Link>
         </div>
       </div>
     
