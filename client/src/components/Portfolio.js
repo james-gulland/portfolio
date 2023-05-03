83957db81023630ccf6c95cb352fbff21c9e9539
James Gulland <jamesgulland@gmail.com>
2023-05-03 22:47:56 +0100

    adding links to grid

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import spectrumScreen from '../assets/spectrum-demo.png'
 import wanderlust from '../assets/wanderlust-crop.png'
 import battleships from '../assets/battleships.png'
@@ -7,43 +8,63 @@ const Portfolio = () => {
 
   return (
     <>
+      {/* 1ST ITEM */}
       <div className="portfolio-item tile-large">
-        <div className="tile-centre">
-          <h3>spectrum</h3>
-          <p className="tile-text">Music app that brings together all your favourite mixes from around the web, including Soundcloud and Youtube.</p>
-          <p className="tile-text">Built a full-stack app from scratch using Python and Django to serve data from a PostgreSQL database, which was then consumed by React.js app.</p>
-        </div>
-        <div className="tile-right">
-          <img id="spectrum-screen" src={spectrumScreen} alt="Spectrum" />
-        </div>
+        <Link to="https://spectrum.gives/" target="_blank" rel="noopener noreferrer"> 
+          <div className="tile-centre">  
+            <h3>spectrum</h3>
+            <p className="tile-text">Music app that brings together all your favourite mixes from around the web, including Soundcloud and Youtube.</p>
+            <p className="tile-text">Built a full-stack app from scratch using Python and Django to serve data from a PostgreSQL database, which was then consumed by React.js app.</p>
+          </div>
+        </Link>
+        <Link to="https://spectrum.gives/" target="_blank" rel="noopener noreferrer">
+          <div className="tile-right">
+            <img id="spectrum-screen" src={spectrumScreen} alt="Spectrum" />
+          </div>
+        </Link>
       </div>
+
+      {/* 2ND ITEM */}
       <div className="portfolio-item tile-small">
-        <div className="tile-vertical">
-          <h3>Wanderlust</h3>
-          <p className="tile-text">Travel website that displays the best destinations based on the current weather in that location right now</p>
-        </div>
-        <div id="image-container">
-          <img id="wanderlust-screen" src={wanderlust} alt="Wanderlust" />
-        </div>
+        <Link to="https://wanderlust-project-3.herokuapp.com/" target="_blank" rel="noopener noreferrer"> 
+          <div className="tile-vertical">
+            <h3>Wanderlust</h3>
+            <p className="tile-text">Travel website that displays the best destinations based on the current weather in that location right now</p>
+          </div>
+          <div id="image-container">
+            <img id="wanderlust-screen" src={wanderlust} alt="Wanderlust" />
+          </div>
+        </Link>
       </div>
+
+      {/* 3RD ITEM */}
       <div className="portfolio-item tile-small">
-        <div className="tile-vertical">
-          <h3>Celebrity Battleships</h3>
-          <p className="tile-text">A tongue-in-cheek battleships game featuring celebrity dictators, built with JavaScript, HTML5 and CSS.</p>
-        </div>
-        <div id="image-container">
-          <img id="battleships-screen" src={battleships} alt="Wanderlust" />
-        </div>
+        <Link to="https://james-gulland.github.io/battleships/" target="_blank" rel="noopener noreferrer">
+          <div className="tile-vertical">
+            <h3>Celebrity Battleships</h3>
+            <p className="tile-text">A tongue-in-cheek battleships game featuring celebrity dictators, built with JavaScript, HTML5 and CSS.</p>
+          </div>
+          <div id="image-container">
+            <img id="battleships-screen" src={battleships} alt="Wanderlust" />
+          </div>
+        </Link>
       </div>
+
+      {/* 4TH ITEM */}
       <div className="portfolio-item tile-large">
-        <div className="tile-left">
-          <img id="spectrum-screen" src={journalinspace} alt="Spectrum" />
-        </div>
-        <div className="tile-centre">
-          <h3>Journal in Space</h3>
-          <p className="tile-text">Experience a daily voyage through space, directly from NASA.</p>
-          <p className="tile-text">Developed a React.js app that consumed the official NASA Picture of the Day API, to resemble a daily journal through space.</p>
-        </div>
+        <Link to="https://journalinspace.com/" target="_blank" rel="noopener noreferrer"> 
+          <div className="tile-left">
+            <img id="spectrum-screen" src={journalinspace} alt="Spectrum" />
+          </div>
+        </Link>
+
+        <Link to="https://journalinspace.com/" target="_blank" rel="noopener noreferrer"> 
+          <div className="tile-centre">
+            <h3>Journal in Space</h3>
+            <p className="tile-text">Experience a daily voyage through space, directly from NASA.</p>
+            <p className="tile-text">Developed a React.js app that consumed the official NASA Picture of the Day API, to resemble a daily journal through space.</p>
+          </div>
+        </Link>
       </div>
     </>
   )
